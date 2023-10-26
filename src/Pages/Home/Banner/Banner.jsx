4336f40474628b70f45ef6b8531473729d8ecf73
Humairaa1 <humaira71bd@gmail.com>
2023-10-26 20:50:11 +0600

    banner overlay and about us section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -7,44 +7,112 @@ import img6 from "../../../assets/images/banner/6.jpg"
 
 const Banner = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full h-[600px]">
             <div id="slide1" className="carousel-item relative w-full">
-                <img src={img1} className="w-full" />
+
+                <img src={img1} className="w-full rounded-xl" />
+
+                <div className="absolute flex items-center h-full left-0 right-0 top-0 text-white rounded-xl bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                    <div className="w-1/2 space-y-5 pl-14">
+                        <h2 className="text-4xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className="flex gap-5">
+                            <button className="btn btn-outline btn-warning">Discover More</button>
+                            <button className="btn btn-outline btn-warning">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide4" className="btn btn-circle">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
-                <img src={img2} className="w-full" />
+                <img src={img2} className="w-full rounded-xl" />
+
+                <div className="absolute flex items-center h-full left-0 right-0 top-0 text-white rounded-xl bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                    <div className="w-1/2 space-y-5 pl-14">
+                        <h2 className="text-4xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className="flex gap-5">
+                            <button className="btn btn-outline btn-warning">Discover More</button>
+                            <button className="btn btn-outline btn-warning">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide1" className="btn btn-circle">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-                <img src={img3} className="w-full" />
+                <img src={img3} className="w-full rounded-xl" />
+
+                <div className="absolute flex items-center h-full left-0 right-0 top-0 text-white rounded-xl bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                    <div className="w-1/2 space-y-5 pl-14">
+                        <h2 className="text-4xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className="flex gap-5">
+                            <button className="btn btn-outline btn-warning">Discover More</button>
+                            <button className="btn btn-outline btn-warning">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide2" className="btn btn-circle">❮</a>
                     <a href="#slide4" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide4" className="carousel-item relative w-full">
-                <img src={img4} className="w-full" />
+                <img src={img4} className="w-full rounded-xl" />
+
+                <div className="absolute flex items-center h-full left-0 right-0 top-0 text-white rounded-xl bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                    <div className="w-1/2 space-y-5 pl-14">
+                        <h2 className="text-4xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className="flex gap-5">
+                            <button className="btn btn-outline btn-warning">Discover More</button>
+                            <button className="btn btn-outline btn-warning">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide3" className="btn btn-circle">❮</a>
                     <a href="#slide5" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide5" className="carousel-item relative w-full">
-                <img src={img5} className="w-full" />
+                <img src={img5} className="w-full rounded-xl" />
+
+                <div className="absolute flex items-center h-full left-0 right-0 top-0 text-white rounded-xl bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                    <div className="w-1/2 space-y-5 pl-14">
+                        <h2 className="text-4xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className="flex gap-5">
+                            <button className="btn btn-outline btn-warning">Discover More</button>
+                            <button className="btn btn-outline btn-warning">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide4" className="btn btn-circle">❮</a>
                     <a href="#slide6" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide6" className="carousel-item relative w-full">
-                <img src={img6} className="w-full" />
+                <img src={img6} className="w-full rounded-xl" />
+
+                <div className="absolute flex items-center h-full left-0 right-0 top-0 text-white rounded-xl bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                    <div className="w-1/2 space-y-5 pl-14">
+                        <h2 className="text-4xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className="flex gap-5">
+                            <button className="btn btn-outline btn-warning">Discover More</button>
+                            <button className="btn btn-outline btn-warning">Latest Project</button>
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute flex justify-end gap-4 transform -translate-y-1/2 left-5 right-5 bottom-0">
                     <a href="#slide5" className="btn btn-circle">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
